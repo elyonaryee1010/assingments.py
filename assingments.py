7a9e83a6711c3048716f2d692195982f79f6ec5a
Elyon <duboisaryee@gmail.com>
2025-11-08 17:12:57 +0000

    Implement input and swap functionality in assingments.py

---
+++ assingments.py
@@ -0,0 +1,18 @@
+#Taking Inputs
+
+a = input("Type Value of A")
+b = input("Type Value of B")
+c = input("Type Value of c")
+
+#swap
+
+temp = a
+a = b
+b = temp
+
+
+#Display results
+
+print(Value of A after the swap", a)
+print(Value of B after the swap", b)
+print(Value of C after the swap", c)
