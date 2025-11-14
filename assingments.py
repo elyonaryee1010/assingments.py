8162ce9d835981b716cf7869aa6000cd84501078
Elyon <duboisaryee@gmail.com>
2025-11-14 16:49:41 +0000

    Add age verification for JASSCO application

--- assingments.py
+++ assingments.py
@@ -16,3 +16,16 @@ b = temp
 print(Value of A after the swap", a)
 print(Value of B after the swap", b)
 print(Value of C after the swap", c)
+
+#Assignment
+
+print("Welcome to Age-Verfications for your JASSCO 2025 Application. Please note that you are to be 18 years before you proceed")
+
+age = int(input("Age:"))
+print("You're age:", age)
+if age >= 18:
+    print("You are eligble. Please proceeed with you're JASSSCO Apllication.")
+else:
+    print("Sorry, You're too underaged to take your JASSCO Exam. Try again when you're older.")
+
+    
