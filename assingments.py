60061f132db02e54d2714afc835fff608db1d3e3
Elyon <duboisaryee@gmail.com>
2025-11-15 17:15:23 +0000

    Enhance functionality with input handling and calculations
    Added input handling, swapping logic, and BMI calculation. Included checks for odd/even numbers and Armstrong number validation.

--- assingments.py
+++ assingments.py
@@ -13,9 +13,9 @@ b = temp
 
 #Display results
 
-print(Value of A after the swap", a)
-print(Value of B after the swap", b)
-print(Value of C after the swap", c)
+print("Value of A after the swap", a)
+print("Value of B after the swap", b)
+print("Value of C after the swap", c)
 
 #Assignment
 
@@ -27,5 +27,99 @@ if age >= 18:
     print("You are eligble. Please proceeed with you're JASSSCO Apllication.")
 else:
     print("Sorry, You're too underaged to take your JASSCO Exam. Try again when you're older.")
+
+# New code
+
+#Taking Inputs
+
+a = input("Type Value of A")
+b = input("Type Value of B")
+c = input("Type Value of c")
+
+#swap
+
+temp = a
+a = b
+b = temp
+
+
+#Display results
+
+print("Value of A after the swap", a)
+print("Value of B after the swap", b)
+print("Value of C after the swap", c)
+
+#Odd or Even
+
+number = int(input("Enter a numer :"))
+print("number is :", number)
+if number%2==0:
+    print("Even Number detected")
+else:
+    print("Odd Number detected")
+
+            #BMI
+weight = int(input("Enter your weight in kilograms :"))
+height = int(input("Enter your weight in cm"))
+BMI = weight/(height/100)**2
+print("Your BMI is :", BMI)
+
+if BMI<18.5:
+     print("Your are underweight")
+elif BMI>=18.5 and BMI<24.9:
+    print("You are normal")
+elif BMI>=25 and BMI<29.9:
+    print("You are overweight")
+else:
+    print("You're obese")
+
+    #Double Check
+
+number = int(input("Enter your number here"))
+print("You're number is:", number)
+if number>=50:
+    if number%2==0:
+      print("Great")
+else:
+     print("Less")
+
+    else:
+        print("nothing_)"
+
+
+    #Time &date
+import datetime
+current_time = datetime.datime.new()
+print("current time here is:", current_time)
+
+
+
+#Assignment
+print("Welcome to Age-Verfications for your JASSCO 2025 Application. Please note that you are to be 18 years before you proceed")
+age = int(input("Age:"))
+print("You're age:", age)
+if age >= 18:
+    print("You are eligble. Please proceeed with you're JASSSCO Apllication.")
+else:
+    print("Sorry, You're too underaged to take your JASSCO Exam. Try again when you're older.")
+    
+
+    
+#15/11/2025
+
+num = 200
+num_str = str(num)
+n = len(num_str)
+total_sum = 0
+for digit_char in num_str:
+    digit = int(digit_char)
+    total_sum += digit**n
+if total_sum == num:
+    print(f"{num} This is an ARMstrong number")
+else:
+    print(f"{num} This isnt armstrong")
+    
+    
+
 
     
